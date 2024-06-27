fce6e41dad3f26e81a61dbb556c05dce61faf449
alvrath <alvrath@gmail.com>
2024-06-27 16:49:41 +0500

    Дошел до детального просмотра и пагинации

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import include, re_path
+#from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('books/', views.BookListView.as_view(), name='books'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^author/$', views.AuthorListView.as_view(), name='authors'),
 ]
-
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre
 
@@ -20,3 +21,18 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
